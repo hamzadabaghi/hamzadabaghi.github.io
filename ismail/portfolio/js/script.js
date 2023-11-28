47f9ff5be160eb15e2b71747204bfdda1f1738d8
Dabaghi-Anass <102183551+Dabaghi-Anass@users.noreply.github.com>
2023-11-28 20:30:02 +0100

    mnin ghatbreq 3la xi lien ghatsed lmenu

--- ismail/portfolio/js/script.js
+++ ismail/portfolio/js/script.js
@@ -3,3 +3,7 @@ const navPhoneMenuToggler = document.querySelector("#menu-toggler");
 
 const toggleHiddenClass = () => navPhoneMenuContainer.classList.toggle("hidden");
 navPhoneMenuToggler.addEventListener("click" , toggleHiddenClass)
+
+navPhoneMenuContainer.querySelector("ul").addEventListener('click' , ()=>{
+    navPhoneMenuContainer.classList.add('hidden')
+})
