ab170af5922b43fd3a257335d60160e3786ff1c3
Dabaghi-Anass <102183551+Dabaghi-Anass@users.noreply.github.com>
2023-11-29 12:07:44 +0100

    fixed menu scrolling issues

--- ismail/portfolio/js/script.js
+++ ismail/portfolio/js/script.js
@@ -1,9 +1,14 @@
 const navPhoneMenuContainer = document.querySelector("#menu-container")
-const navPhoneMenuToggler = document.querySelector("#menu-toggler");
+const navPhoneMenuToggler = document.querySelectorAll(".menu-toggler");
 
-const toggleHiddenClass = () => navPhoneMenuContainer.classList.toggle("hidden");
-navPhoneMenuToggler.addEventListener("click" , toggleHiddenClass)
+const toggleHiddenClass = () => {
+    navPhoneMenuContainer.classList.toggle("hidden");
+}
+navPhoneMenuToggler.forEach(menuBtn => menuBtn.addEventListener("click" , toggleHiddenClass));
 
 navPhoneMenuContainer.querySelector("ul").addEventListener('click' , ()=>{
+    navPhoneMenuContainer.classList.add('hidden')
+})
+navPhoneMenuContainer.querySelector(".app-brand").addEventListener('click' , ()=>{
     navPhoneMenuContainer.classList.add('hidden')
 })
